e5fee2a7957a899c538deecfabe99b1de59bc3e4
Kasper-Loontjens <112395890+Kasper-Loontjens@users.noreply.github.com>
2022-10-06 09:57:30 +0200

    Add files via upload

--- main.java
+++ main.java
@@ -1,27 +1,27 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class main {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		while (true) {
 			int numberOfTries =0;
 			int selectedNumber = -1;
 			int randomNumber = (int) Math.round(Math.random()*100);
 			System.out.println("Write a number. \n___________");
 			while (isTheNumberCorrect(randomNumber, selectedNumber)) {
-				selectedNumber = sc.nextInt();
+				selectedNumber = takeNumber();
 				numberOfTries++;
 				System.out.println(returnMessage(selectedNumber, randomNumber, numberOfTries));
 			}
-			System.out.println("___________ \nDo you want to play again? \n1. Yes \n2. No");
-			if (sc.nextInt()==2) {
+			System.out.println("___________ \nDo you want to play again?");
+			if ((sc.next().toLowerCase().trim()).equals("no")) {
 				System.out.println("Ok, bye.");
 				break;
 			}
 		}
-		
 	}
 	static boolean isTheNumberCorrect(int i, int x) {
 		if (i == x) {
@@ -37,6 +37,16 @@ public class main {
 			return("Lower");
 		}else {
 			return ("Higher");
+		}
+	}
+	static int takeNumber() {
+		while (true) {
+			try {
+				return sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Bugger off");
+				sc.next();
+			}
 		}
 	}
 
